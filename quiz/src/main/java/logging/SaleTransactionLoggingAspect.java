67f4c217030ad0115c3c095c6d343d0f562895ff
IsmailAbed <152910128+IsmailAbed@users.noreply.github.com>
2024-10-19 00:52:54 +0300

    Update SaleTransactionLoggingAspect.java

--- quiz/src/main/java/logging/SaleTransactionLoggingAspect.java
+++ quiz/src/main/java/logging/SaleTransactionLoggingAspect.java
@@ -1,21 +1,20 @@
-//package logging;
-//
-//
-//import org.aspectj.lang.annotation.AfterReturning;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.stereotype.Component;
-//
-//@Aspect
-//@Component
-//public class SaleTransactionLoggingAspect {
-//
-//    private static final Logger logger = LoggerFactory.getLogger(SaleTransactionLoggingAspect.class);
-//
-//    @AfterReturning(pointcut = "execution(* com.quiz._quiz.services.SaleService.updateSaleTransaction(..))", returning = "result")
-//    public void logAfterSaleTransactionUpdate(Object result) {
-//        logger.info("Sale transaction updated: {}", result);
-//    }
-//}
-//
+package logging;
+
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+Component
+public class SaleTransactionLoggingAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(SaleTransactionLoggingAspect.class);
+
+    @AfterReturning(pointcut = "execution(* com.quiz._quiz.services.SaleService.updateSaleTransaction(..))", returning = "result")
+    public void logAfterSaleTransactionUpdate(Object result) {
+        logger.info("Sale transaction updated: {}", result);
+    }
+}
+
